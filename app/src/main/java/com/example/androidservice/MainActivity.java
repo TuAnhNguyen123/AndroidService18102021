5d09d773d8d7cab74af664ce1847e44bceabd7ae
JoseNguyen <abc@example.com>
2022-01-03 18:34:34 +0700

    Initial commit

---
+++ app/src/main/java/com/example/androidservice/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.androidservice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+///ForceGroundService
+    Button mBtnStartService, mBtnStopService;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mBtnStartService = findViewById(R.id.buttonStartService);
+        mBtnStopService = findViewById(R.id.buttonStoptService);
+
+        mBtnStartService.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,MyService.class);
+                startService(intent);
+            }
+        });
+
+    }
+}
